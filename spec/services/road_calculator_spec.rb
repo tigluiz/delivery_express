cf05a6ea7b60edff520c173c0a304ccc0f34d576
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-17 11:17:58 -0300

    Add test for what we want to raod calculator returns

--- spec/services/road_calculator_spec.rb
+++ spec/services/road_calculator_spec.rb
@@ -3,9 +3,19 @@ require File.expand_path( File.join( File.dirname(__FILE__), '../../app/services
 describe RoadCalculator do
 
   context "#apply" do
-    context "with"
-    it "should have map location array" do
-      expect(lambda{described_class.new("something").apply}).to raise_error
+    context "with wrong params" do
+      it "should raise error" do
+        expect(lambda{described_class.new("something", "something").apply}).to raise_error
+      end
+    end
+    context "with correct params" do
+      context "With short roads" do
+        let(:user_choice) {{origin: "A", destiny: "B", autonomy: 10, gas_value: "2.5"}}
+        let(:roads) {["A B 10", "B C 15"]}
+        it "returns best road" do
+          expect(described_class.new(user_choice, roads).apply).to eql({road: "AB", cost: "2.5"})
+        end
+      end
     end
   end
 end
