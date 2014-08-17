f2b2b13070cbd24318bf6261c307be6fb3afd47f
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-17 12:56:22 -0300

    Add behavior to choose roads with simple two points

--- app/services/road_calculator.rb
+++ app/services/road_calculator.rb
@@ -2,16 +2,31 @@ class RoadCalculator
   def initialize(chossen_road,roads)
     @user_road_info = chossen_road
     @roads = roads
+    @distance = 0
+    @road = ""
   end
 
   def apply
     raise "Must receive map location array" unless @roads.is_a?(Array)
+    choose_best_road
   end
 
   private
 
   def choose_best_road
-    
+    chossen = @roads.map{|road| road if( road.join(" ").match(@user_road_info[:origin]) && road.join(" ").match(@user_road_info[:destiny]))}.compact.flatten
+    @road = chossen[0] + chossen[1]
+    @road.reverse! if need_reverse?
+    @distance = @distance += chossen[2].to_i
+    {road: @road, cost: calculate_cost}
+  end
+
+  def calculate_cost
+    BigDecimal(@distance) / BigDecimal(@user_road_info[:autonomy]) * BigDecimal(@user_road_info[:gas_value])
+  end
+
+  def need_reverse?
+    !!(!@road.match(@user_road_info[:origin] + @user_road_info[:destiny]))
   end
 
 end
--- spec/services/road_calculator_spec.rb
+++ spec/services/road_calculator_spec.rb
@@ -1,5 +1,7 @@
 require File.expand_path( File.join( File.dirname(__FILE__), '../../app/services/road_calculator' ) )
-
+require 'bigdecimal'
+require "pry"
+# require "rails_helper"
 describe RoadCalculator do
 
   context "#apply" do
@@ -9,11 +11,18 @@ describe RoadCalculator do
       end
     end
     context "with correct params" do
-      context "With short roads" do
+      context "With short road" do
         let(:user_choice) {{origin: "A", destiny: "B", autonomy: 10, gas_value: "2.5"}}
-        let(:roads) {["A B 10", "B C 15"]}
+        let(:roads) {[["A", "B", "10"], ["B", "C", "15"]]}
         it "returns best road" do
-          expect(described_class.new(user_choice, roads).apply).to eql({road: "AB", cost: "2.5"})
+          expect(described_class.new(user_choice, roads).apply).to eql({road: "AB", cost: 2.5})
+        end
+      end
+      context "With invert short road" do
+        let(:user_choice) {{origin: "B", destiny: "A", autonomy: 10, gas_value: "2.5"}}
+        let(:roads) {[["A", "B", "10"], ["B", "C", "15"]]}
+        it "returns best road" do
+          expect(described_class.new(user_choice, roads).apply).to eql({road: "BA", cost: 2.5})
         end
       end
     end
