7ed35999f32b482838f42d06e991114f60a209ad
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-17 17:19:22 -0300

    Add method for parse road info saved

--- app/models/map_location.rb
+++ app/models/map_location.rb
@@ -1,3 +1,7 @@
 class MapLocation < ActiveRecord::Base
   validates :name, :road_info, presence: true
+
+  def parse_road_info
+    road_info.split(",").map{|road| road.split(" ")}
+  end
 end
--- spec/models/map_location_spec.rb
+++ spec/models/map_location_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 RSpec.describe MapLocation, :type => :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:road_info) }
+
+  context "#parse_road_info" do
+    subject {described_class.new(name: "example",road_info: "A B 10, B D 15, A C 20")}
+    it "return parsed road info" do
+      expect(subject.parse_road_info).to eql([["A", "B", "10"], ["B", "D", "15"], ["A", "C", "20"]])
+    end
+  end
 end
