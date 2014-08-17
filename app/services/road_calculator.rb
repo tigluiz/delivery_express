0bb80ef3dc4e4dcad432fa63f94d5c533e7cf527
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-17 16:13:42 -0300

    Add to road calculator behavior to choose best way

--- app/services/road_calculator.rb
+++ app/services/road_calculator.rb
@@ -4,6 +4,7 @@ class RoadCalculator
     @roads = roads
     @distance = 0
     @road = ""
+    @inverted = chossen_road[:origin] > chossen_road[:destiny]
   end
 
   def apply
@@ -14,11 +15,35 @@ class RoadCalculator
   private
 
   def choose_best_road
-    chossen = @roads.map{|road| road if( road.join(" ").match(@user_road_info[:origin]) && road.join(" ").match(@user_road_info[:destiny]))}.compact.flatten
-    @road = chossen[0] + chossen[1]
-    @road.reverse! if need_reverse?
-    @distance = @distance += chossen[2].to_i
+    @road += @user_road_info[:origin]
+    seek_road(@roads,@user_road_info[:origin], @user_road_info[:destiny])
     {road: @road, cost: calculate_cost}
+  end
+
+  def seek_road(rods , o , oo)
+    _road = (rods).map{|road| road if( road.join(" ").match(o) && road.join(" ").match(oo))}.compact
+    if _road.size == 1
+      _road = _road.flatten
+      if @inverted
+        @road += _road[1] unless @road.include?(_road[1])
+        @road += _road[0] unless @road.include?(_road[0])
+      else
+        @road += _road[0] unless @road.include?(_road[0])
+        @road += _road[1] unless @road.include?(_road[1])
+      end
+      @distance = @distance += _road[2].to_i
+      return _road
+    else
+      _roads = @roads.map{|road| road if( road.join(" ").match(@road))}.compact
+      aroad = _roads.sort{|a,b| a[2] <=> b[2]}.first
+      aroads = @roads.delete_if{|f| f.join(" ").include?(@road)}
+      @distance = @distance += aroad[2].to_i
+      if @inverted
+        seek_road(aroads, aroads[0][1], oo)
+      else
+        seek_road(aroads, aroads[0][0], oo)
+      end
+    end
   end
 
   def calculate_cost
--- spec/services/road_calculator_spec.rb
+++ spec/services/road_calculator_spec.rb
@@ -11,18 +11,39 @@ describe RoadCalculator do
       end
     end
     context "with correct params" do
-      context "With short road" do
+      context "and same road" do
         let(:user_choice) {{origin: "A", destiny: "B", autonomy: 10, gas_value: "2.5"}}
         let(:roads) {[["A", "B", "10"], ["B", "C", "15"]]}
         it "returns best road" do
           expect(described_class.new(user_choice, roads).apply).to eql({road: "AB", cost: 2.5})
         end
       end
-      context "With invert short road" do
+      context "and same inverted road" do
         let(:user_choice) {{origin: "B", destiny: "A", autonomy: 10, gas_value: "2.5"}}
         let(:roads) {[["A", "B", "10"], ["B", "C", "15"]]}
         it "returns best road" do
           expect(described_class.new(user_choice, roads).apply).to eql({road: "BA", cost: 2.5})
+        end
+      end
+      context "With two distance points" do
+        let(:user_choice) {{origin: "A", destiny: "D", autonomy: 10, gas_value: "2.5"}}
+        let(:roads) {[["A", "B", "10"], ["B", "D", "15"], ["A", "C", "20"], ["C", "D", "30"]]}
+        it "returns best road" do
+          expect(described_class.new(user_choice, roads).apply).to eql({road: "ABD", cost: BigDecimal("6.25")})
+        end
+      end
+      context "With two distance inverted points" do
+        let(:user_choice) {{origin: "D", destiny: "A", autonomy: 10, gas_value: "2.5"}}
+        let(:roads) {[["A", "B", "10"], ["B", "D", "15"], ["A", "C", "20"], ["C", "D", "30"]]}
+        it "returns best road" do
+          expect(described_class.new(user_choice, roads).apply).to eql({road: "DBA", cost: BigDecimal("6.25")})
+        end
+      end
+      context "With tree distance points", focus: true do
+        let(:user_choice) {{origin: "A", destiny: "E", autonomy: 10, gas_value: "2.5"}}
+        let(:roads) {[["A", "B", "10"], ["B", "D", "15"], ["A", "C", "20"], ["C", "D", "30"],["B", "E", "50"], ["D", "E", "30"]]}
+        it "returns best road" do
+          expect(described_class.new(user_choice, roads).apply).to eql({road: "ABE", cost: BigDecimal("15")})
         end
       end
     end
