9a0059fe10b0c8f2e63b6c74c0a46cb51b407fbf
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-17 11:17:32 -0300

    Add more information on road calculator initialize

--- app/services/road_calculator.rb
+++ app/services/road_calculator.rb
@@ -1,10 +1,17 @@
 class RoadCalculator
-  def initialize(roads)
+  def initialize(chossen_road,roads)
+    @user_road_info = chossen_road
     @roads = roads
   end
 
   def apply
     raise "Must receive map location array" unless @roads.is_a?(Array)
+  end
+
+  private
+
+  def choose_best_road
+    
   end
 
 end
